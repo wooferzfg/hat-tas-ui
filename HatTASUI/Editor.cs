3a1eb5f1aef60ed1d49a8da3a0ea2ebfa6c361ef
Sergey Papushin <spapushin@gmail.com>
2018-05-28 12:23:12 -0500

    Add some interactivity to left stick

--- HatTASUI/Editor.cs
+++ HatTASUI/Editor.cs
@@ -12,9 +12,107 @@ namespace HatTASUI
 {
     public partial class Editor : Form
     {
+        private int _LeftX;
+        private int _LeftY;
+
+        public int LeftX
+        {
+            get
+            {
+                return _LeftX;
+            }
+            set
+            {
+                _LeftX = Math.Max(Math.Min(value, 65535), 0);
+                txtLeftX.Value = _LeftX;
+                trkLeftX.Value = _LeftX;
+            }
+        }
+        public int LeftY
+        {
+            get
+            {
+                return _LeftY;
+            }
+            set
+            {
+                _LeftY = Math.Max(Math.Min(value, 65535), 0);
+                txtLeftY.Value = _LeftY;
+                trkLeftY.Value = -_LeftY;
+            }
+        }
+
         public Editor()
         {
             InitializeComponent();
+        }
+
+        private void btnLeftLeft_Click(object sender, EventArgs e)
+        {
+            LeftX = 0;
+            LeftY = 32767;
+        }
+
+        private void btnLeftDown_Click(object sender, EventArgs e)
+        {
+            LeftX = 32767;
+            LeftY = 65535;
+        }
+
+        private void btnLeftUp_Click(object sender, EventArgs e)
+        {
+            LeftX = 32767;
+            LeftY = 0;
+        }
+
+        private void btnLeftRight_Click(object sender, EventArgs e)
+        {
+            LeftX = 65535;
+            LeftY = 32767;
+        }
+
+        private void btnLeftDownLeft_Click(object sender, EventArgs e)
+        {
+            LeftX = 0;
+            LeftY = 65535;
+        }
+
+        private void btnLeftDownRight_Click(object sender, EventArgs e)
+        {
+            LeftX = 65535;
+            LeftY = 65535;
+        }
+
+        private void btnLeftUpLeft_Click(object sender, EventArgs e)
+        {
+            LeftX = 0;
+            LeftY = 0;
+        }
+
+        private void btnLeftUpRight_Click(object sender, EventArgs e)
+        {
+            LeftX = 65535;
+            LeftY = 0;
+        }
+
+        private void txtLeftY_ValueChanged(object sender, EventArgs e)
+        {
+            LeftY = (int)txtLeftY.Value;
+        }
+
+        private void txtLeftX_ValueChanged(object sender, EventArgs e)
+        {
+            LeftX = (int)txtLeftX.Value;
+        }
+
+        private void trkLeftX_ValueChanged(object sender, EventArgs e)
+        {
+            LeftX = trkLeftX.Value;
+        }
+
+        private void trkLeftY_ValueChanged(object sender, EventArgs e)
+        {
+            LeftY = -trkLeftY.Value;
         }
     }
 }
